4e04eab209b0ca77e077253f2716966063888322
TheP2WKing <tdziomba@hotmail.com>
2023-05-03 21:56:03 +0200

    rebrand from rf to flux tools

--- src/main/java/net/thep2wking/exastris/init/ExAstrisRAItems.java
+++ src/main/java/net/thep2wking/exastris/init/ExAstrisRAItems.java
@@ -10,8 +10,8 @@ import net.thep2wking.exastris.content.item.hammer.ItemFluxInfusedHammer;
 public class ExAstrisRAItems {
     public static final List<Item> ITEMS_RA = new ArrayList<Item>();
 
-        public static final Item HAMMER_FLUX = new ItemFluxInfusedHammer("hammer_rf", ExAstrisTabs.EXASTRIS_TAB,
+        public static final Item HAMMER_FLUX = new ItemFluxInfusedHammer("hammer_flux", ExAstrisTabs.EXASTRIS_TAB,
                        320000, 4000, 400, 800, 4, 6, 16f);
-        public static final Item CROOK_FLUX = new ItemFluxInfusedCrook("crook_rf", ExAstrisTabs.EXASTRIS_TAB, 320000,
+        public static final Item CROOK_FLUX = new ItemFluxInfusedCrook("crook_flux", ExAstrisTabs.EXASTRIS_TAB, 320000,
                         4000, 200, 400, 2, 4, 6f);
 }
